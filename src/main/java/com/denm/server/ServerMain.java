af400c1a6224fa9e0e0541354a6470e1bbe2df5b
Denis Makarov <denm2000@gmail.com>
2015-12-17 20:07:24 +0200

    feature/HW01_Test1Impl : test after Lesson 1 - DONE

--- src/main/java/com/denm/server/ServerMain.java
+++ src/main/java/com/denm/server/ServerMain.java
@@ -5,15 +5,20 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import java.util.logging.Logger;
+
 /**
  * Created by DENM on 14.12.2015.
  */
 public class ServerMain {
+    private final static Logger logger = Logger.getGlobal();
+
     public static void main(String[] args) throws Exception {
+        logger.info("Server started");
         AllRequestServlet allRequestsServlet = new AllRequestServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror/*");
 
         Server server = new Server(8080);
         server.setHandler(context);
